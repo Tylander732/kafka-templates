e99f524e49d1ba9adace788522a4427ac0b84acb
Tylander <tyler.harding.sde@gmail.com>
2024-05-23 09:58:07 -0700

    dependencies

--- java/src/main/java/com/tylander/template/kafka/controller/KafkaController.java
+++ java/src/main/java/com/tylander/template/kafka/controller/KafkaController.java
@@ -2,9 +2,11 @@ package com.tylander.template.kafka.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class KafkaController {
 
 	@GetMapping("response")
