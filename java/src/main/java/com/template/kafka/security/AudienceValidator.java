96ade2a3d7dc34a591b5493a136b5980b801d7fc
Tylander <tyler.harding.sde@gmail.com>
2024-05-28 21:17:59 -0700

    additional dependencies. Adding security and audience validator. Issues with imports

---
+++ java/src/main/java/com/template/kafka/security/AudienceValidator.java
@@ -0,0 +1,7 @@
+package com.template.kafka.security;
+
+// import org.springframework.security.oauth2.core.OAuth2Error;
+//
+// public class AudienceValidator implements Oauth2TokenValidator<JWT> {
+//
+// }
